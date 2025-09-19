b937a4501fba0e12512a1d79e9a40e941201263b
Rohit Goyal <rohitgoyal4220@gmail.com>
2025-09-19 12:40:45 +0530

    improved the connectivity with postgres

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -4,12 +4,15 @@ dotenv.config();
 
 const { Pool} = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool ({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 pool.connect()
